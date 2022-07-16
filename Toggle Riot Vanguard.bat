672b8a638d2b1ba7dface05a127ff48adc8f557b
Vudge <70611009+Vudge@users.noreply.github.com>
2022-07-16 18:14:42 +0000

    Update Toggle Riot Vanguard.bat

--- Toggle Riot Vanguard.bat
+++ Toggle Riot Vanguard.bat
@@ -4,9 +4,9 @@ if exist vgk.sys (
 	rename vgc.exe vgc_off.exe
     	rename vgk.sys vgk_off.sys
     	rename vgtray.exe vgtray_off.exe
-    	cscript "C:\%AppData%\Microsoft\Windows\Start Menu\Programs\Scripts\Disable Vanguard.vbs"
+    	cscript "C:\Program Files\Riot Vanguard\Scripts\Disable Vanguard.vbs"
 )else if exist vgk_off.sys (
     	rename vgc_off.exe vgc.exe
     	rename vgk_off.sys vgk.sys
     	rename vgtray_off.exe vgtray.exe
-    	cscript "C:\Users\Vudgekek\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Utilities\enable_vanguard_restart.vbs"
+    	cscript "C:\Program Files\Riot Vanguard\Scripts\Enable Vanguard.vbs"
