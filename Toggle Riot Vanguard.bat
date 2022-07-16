c8c210332ce07e2109e0b4ce2e1ef484bdc3232a
Vudge <70611009+Vudge@users.noreply.github.com>
2022-07-16 19:05:39 +0000

    Introduce proper syntax (thx https://github.com/amitxv/) and disable services directly

--- Toggle Riot Vanguard.bat
+++ Toggle Riot Vanguard.bat
@@ -1,13 +1,15 @@
-cd /d "C:\Program Files\Riot Vanguard"
+pushd "C:\Program Files\Riot Vanguard"
 
-if exist vgk.sys (
-	rename vgc.exe vgc_off.exe
-    	rename vgk.sys vgk_off.sys
-    	rename vgtray.exe vgtray_off.exe
-    	cscript "C:\Program Files\Riot Vanguard\Scripts\Disable Vanguard.vbs"
-)else if exist vgk_off.sys (
-    	rename vgc_off.exe vgc.exe
-    	rename vgk_off.sys vgk.sys
-    	rename vgtray_off.exe vgtray.exe
-    	cscript "C:\Program Files\Riot Vanguard\Scripts\Enable Vanguard.vbs"
+if exist "vgk.sys" (
+    ren vgk.sys vgk_off.sys
+    ren vgc.exe vgc_off.exe
+    ren vgtray.exe vgtray_off.exe
+    "sc config vgc start= disabled & sc config vgk start= disabled & net stop vgc & net stop vgk & taskkill /IM vgtray.exe"
+) else (
+    if exist "vgk_off.sys" (
+        ren vgk_off.sys vgk.sys
+        ren vgc_off.exe vgc.exe
+        ren vgtray_off.exe vgtray.exe
+        "sc config vgc start= demand & sc config vgk start= system & shutdown /r /f /t 00"
+    ) 
 )
