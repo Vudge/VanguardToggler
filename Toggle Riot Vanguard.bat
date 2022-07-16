32fe05906bed79c6eaaa563f367aa002a4abcddc
Vudge <70611009+Vudge@users.noreply.github.com>
2022-07-16 19:14:08 +0000

    Make silent

--- Toggle Riot Vanguard.bat
+++ Toggle Riot Vanguard.bat
@@ -5,12 +5,12 @@ if exist "vgk.sys" (
     ren vgk.sys vgk_off.sys
     ren vgc.exe vgc_off.exe
     ren vgtray.exe vgtray_off.exe
-    "sc config vgc start= disabled & sc config vgk start= disabled & net stop vgc & net stop vgk & taskkill /IM vgtray.exe"
+    sc config vgc start= disabled & sc config vgk start= disabled & net stop vgc & net stop vgk & taskkill /IM vgtray.exe
 ) else (
     if exist "vgk_off.sys" (
         ren vgk_off.sys vgk.sys
         ren vgc_off.exe vgc.exe
         ren vgtray_off.exe vgtray.exe
-        "sc config vgc start= demand & sc config vgk start= system & shutdown /r /f /t 00"
+        sc config vgc start= demand & sc config vgk start= system & shutdown /r /f /t 00
     ) 
 )
